d040350e5b2f8b629fd307e185c1cab83f8b9e2e
Lukas Wenner <lukas.wenner@web.de>
2021-11-11 19:18:43 +0100

    Failing test for #12

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -15,4 +15,13 @@ describe "Show author index page", type: :feature do
     visit authors_path
     expect(page).to have_link 'Edit', href: edit_author_path(@author)
   end
+
+  it "should have delete link and delete the author" do
+    @author = FactoryBot.create :author
+    visit authors_path
+    expect(page).to have_link 'Delete'
+    expect(Author.count).to eq(1)
+    find('a[data-method="delete"]').click
+    expect(Author.count).to eq(0)
+  end
 end
