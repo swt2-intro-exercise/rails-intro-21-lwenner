4ed07c8dee4d9ebb91a60670f4b1360c4080c798
Lukas Wenner <lukas.wenner@web.de>
2021-11-11 21:12:43 +0100

    Failing test for #20

--- spec/features/papers/index_paper_spec.rb
+++ spec/features/papers/index_paper_spec.rb
@@ -24,4 +24,18 @@ describe "Show author index page", type: :feature do
     find('a[data-method="delete"]').click
     expect(Paper.count).to eq(0)
   end
+
+  it "should filter the papers" do
+    @paper = Paper.new(title: "COMPUTING MACHINERY AND INTELLIGENCE", venue: "Mind 49: 433-460", year: 1950)
+    @paper.save
+    @paper2 = Paper.new(title: "Voll tolles Paper", venue: "cool", year: 2020)
+    @paper2.save
+    visit '/papers?year=2020'
+    expect(page).to have_text("Voll tolles Paper")
+    expect(page).to have_text("cool")
+    expect(page).to have_text("2020")
+    expect(page).to_not have_text("COMPUTING MACHINERY AND INTELLIGENCE")
+    expect(page).to_not have_text("Mind 49: 433-460")
+    expect(page).to_not have_text("1950")
+  end
 end
