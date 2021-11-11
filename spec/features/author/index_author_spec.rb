89a347dda5d840fe88816fbe462736920cf07a88
Lukas Wenner <lukas.wenner@web.de>
2021-11-11 19:09:45 +0100

    Failing test for #11

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -9,4 +9,10 @@ describe "Show author index page", type: :feature do
     expect(page).to have_text("http://wikipedia.de/Alan_Turing")
     expect(page).to have_link 'New', href: new_author_path
   end
+
+  it "should have edit link" do
+    @author = FactoryBot.create :author
+    visit authors_path
+    expect(page).to have_link 'Edit', href: edit_author_path(@author)
+  end
 end
