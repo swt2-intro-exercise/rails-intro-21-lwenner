b6d0bf4db5ad67eacb69f81aef4ae0b8b029b6c8
Lukas Wenner <lukas.wenner@web.de>
2021-11-11 20:14:37 +0100

    Failing tests for #16

--- spec/features/papers/new_paper_spec.rb
+++ spec/features/papers/new_paper_spec.rb
@@ -33,6 +33,11 @@ describe "New paper page", type: :feature do
     expect(@paper).to_not be_valid
   end
 
+  it "should return not valid because year is empty" do
+    @paper = Paper.new(title: "COMPUTING MACHINERY AND INTELLIGENCE", venue: "Mind 49: 433-460")
+    expect(@paper).to_not be_valid
+  end
+
   it "should return valid" do
     @paper = Paper.new(title: "COMPUTING MACHINERY AND INTELLIGENCE", venue: "Mind 49: 433-460", year: 1950)
     expect(@paper).to be_valid
@@ -40,19 +45,11 @@ describe "New paper page", type: :feature do
 
   it "should show errors when creating" do
     visit new_paper_path
-    page.fill_in 'paper[venue]', with: "Mind 49: 433-460"
-    page.fill_in 'paper[year]', with: 1950
     find('input[type="submit"]').click
 
     expect(page).to have_text("Title can't be blank")
-  end
-
-  it "should show errors when creating" do
-    visit new_paper_path
-    page.fill_in 'paper[title]', with: 'COMPUTING MACHINERY AND INTELLIGENCE'
-    page.fill_in 'paper[year]', with: 1950
-    find('input[type="submit"]').click
-
     expect(page).to have_text("Venue can't be blank")
+    expect(page).to have_text("Year can't be blank")
+    expect(page).to have_text("Year is not a number")
   end
 end
