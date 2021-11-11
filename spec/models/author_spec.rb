bcb09470f9673be49027434add007ca752c97c8f
Lukas Wenner <lukas.wenner@web.de>
2021-11-11 20:45:18 +0100

    add author relation tests

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -8,4 +8,15 @@ RSpec.describe Author, type: :model do
     expect(author.homepage).to eq("http://wikipedia.de/Alan_Turing")
     expect(author.name).to eq("Alan Turing")
   end
+
+  it "should have a author relation" do
+    @author = Author.new(first_name: "Alan", last_name: "Turing", homepage: "http://wikipedia.de/Alan_Turing")
+    expect(@author.papers.length).to eq(0)
+  end
+
+  it "should have an author in the relation" do
+    @paper = FactoryBot.create :paper
+    @author = @paper.authors.create(first_name: "Alan", last_name: "Turing", homepage: "http://wikipedia.de/Alan_Turing")
+    expect(@author.papers.length).to eq(1)
+  end
 end
