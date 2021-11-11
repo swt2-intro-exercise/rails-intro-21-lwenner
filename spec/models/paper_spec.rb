d4cf124521447894e6a0e5c4b2094194dffb9a69
Lukas Wenner <lukas.wenner@web.de>
2021-11-11 20:30:48 +0100

    Failing tests for #17

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -7,4 +7,15 @@ RSpec.describe Paper, type: :model do
     expect(paper.venue).to eq("Mind 49: 433-460")
     expect(paper.year).to eq(1950)
   end
+
+  it "should have a author relation" do
+    @paper = Paper.new(title: "COMPUTING MACHINERY AND INTELLIGENCE", venue: "Mind 49: 433-460", year: 1950)
+    expect(@paper.authors.length).to eq(0)
+  end
+
+  it "should have an author in the relation" do
+    @author = FactoryBot.create :author
+    @paper = @author.papers.create(title: "COMPUTING MACHINERY AND INTELLIGENCE", venue: "Mind 49: 433-460", year: 1950)
+    expect(@paper.authors.length).to eq(1)
+  end
 end
